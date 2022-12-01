b6dd61357adf32a86d00a244b619a61811246b5d
Nuno Barreiro <nuno.barreiro@outlook.com>
2022-12-01 22:56:25 +0000

    solve day 01 - part one

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -1,5 +1,14 @@
 defmodule Day01 do
-  def run(_input) do
+  def run_part_one(input) do
+    [ head | _tail ] = String.split(input, ~r/\n/)
+    |> Enum.map(&(parse_entry(&1)))
+    |> Enum.chunk_by(&(&1 == 0))
+    |> Enum.map(&(Enum.sum(&1)))
+    |> Enum.sort(:desc)
 
+    head
   end
+
+  defp parse_entry(""), do: 0
+  defp parse_entry(str), do: String.to_integer(str)
 end
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -1,11 +1,11 @@
 defmodule Day01Test do
   use ExUnit.Case
 
-  test "Day 01 Test Case" do
+  test "Day 01 - Part One" do
     expected_result = 24_000
     test_input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 
-    assert expected_result == Day01.run(test_input)
+    assert expected_result == Day01.run_part_one(test_input)
   end
 
 end
