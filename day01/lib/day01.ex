dc38c7dfc8846c62f8aecf06cbbbd8352d5cfd72
Nuno Barreiro <nuno.barreiro@outlook.com>
2022-12-01 22:58:17 +0000

    refactor day one - part one

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -1,13 +1,17 @@
 defmodule Day01 do
   def run_part_one(input) do
-    [ head | _tail ] = String.split(input, ~r/\n/)
-    |> Enum.map(&(parse_entry(&1)))
-    |> Enum.chunk_by(&(&1 == 0))
-    |> Enum.map(&(Enum.sum(&1)))
-    |> Enum.sort(:desc)
+    [head | _tail] = get_elves_calories_list(input)
 
     head
   end
+
+  defp get_elves_calories_list(input),
+    do:
+      String.split(input, ~r/\n/)
+      |> Enum.map(&parse_entry(&1))
+      |> Enum.chunk_by(&(&1 == 0))
+      |> Enum.map(&Enum.sum(&1))
+      |> Enum.sort(:desc)
 
   defp parse_entry(""), do: 0
   defp parse_entry(str), do: String.to_integer(str)
