f3e6e0cf9eaa5d4b4e625057ae46eac2e16cc88e
Nuno Barreiro <nuno.barreiro@outlook.com>
2022-12-03 23:58:03 +0000

    solve day 03 - part two

--- day03/lib/day03.ex
+++ day03/lib/day03.ex
@@ -7,6 +7,23 @@ defmodule Day03 do
     |> Enum.sum()
   end
 
+  def run_part_two(file_input) do
+    String.split(file_input, ~r/\n/)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&(find_group_badge(&1)))
+    |> List.flatten()
+    |> Enum.map(&(calculate_priority(&1)))
+    |> Enum.sum()
+  end
+
+  defp find_group_badge([ r_one, r_two, r_three ] = _rucksacks) do
+    String.graphemes(r_one)
+    |> MapSet.new()
+    |> MapSet.intersection(String.graphemes(r_two) |> MapSet.new())
+    |> MapSet.intersection(String.graphemes(r_three) |> MapSet.new())
+    |> Enum.to_list()
+  end
+
   defp calculate_priority(item) do
     subtract_by = if Regex.match?(~r/[a-z]/, item), do: 96, else: 38
     char_code = String.to_charlist(item) |> hd
--- day03/test/day03_test.exs
+++ day03/test/day03_test.exs
@@ -8,4 +8,10 @@ defmodule Day03Test do
     assert Day03.run_part_one(input) === expected_result
   end
 
+  test "part two" do
+    input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw"
+    expected_result = 70
+
+    assert Day03.run_part_two(input) === expected_result
+  end
 end
