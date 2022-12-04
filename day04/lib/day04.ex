20e1a24dafb15e45ac763ae68ebaa7b7b46d5c69
Nuno Barreiro <nuno.barreiro@outlook.com>
2022-12-04 19:31:19 +0000

    solve day 04 - part one

---
+++ day04/lib/day04.ex
@@ -0,0 +1,25 @@
+defmodule Day04 do
+  def run_part_one(input) do
+    String.split(input, "\n")
+    |> Enum.map(&get_pair_assignents(&1))
+    |> Enum.map(&assignment_fully_overlaps?(&1))
+    |> Enum.count(&(&1 == true))
+  end
+
+  def get_pair_assignents(input_line) do
+    String.split(input_line, ",")
+    |> Enum.map(&(create_range(&1)))
+  end
+
+  def create_range(pair_str) do
+    [from, to] = String.split(pair_str, "-")
+    Range.new(from |> String.to_integer(), to |> String.to_integer())
+  end
+
+  def assignment_fully_overlaps?([first_assignments, second_assignments] = _pair_assignments) do
+    first_in_sec = MapSet.subset?(MapSet.new(first_assignments), MapSet.new(second_assignments))
+    sec_in_first = MapSet.subset?(MapSet.new(second_assignments), MapSet.new(first_assignments))
+
+    first_in_sec || sec_in_first
+  end
+end
---
+++ day04/test/day04_test.exs
@@ -0,0 +1,17 @@
+defmodule Day04Test do
+  use ExUnit.Case
+
+  test "part one" do
+    input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+    expected_result = 2
+
+    assert Day04.run_part_one(input) == expected_result
+  end
+
+  # test "part two" do
+  #   input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+  #   expected_result = 2
+
+  #   assert Day04.run_part_one(input) == expected_result
+  # end
+end
