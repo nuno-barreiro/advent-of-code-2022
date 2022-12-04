7b8d3dfc1d628fc11fb45ca7afd9c51fcda6f81e
Nuno Barreiro <nuno.barreiro@outlook.com>
2022-12-04 19:37:30 +0000

    solve day 04 - part two

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -6,6 +6,13 @@ defmodule Day04 do
     |> Enum.count(&(&1 == true))
   end
 
+  def run_part_two(input) do
+    String.split(input, "\n")
+    |> Enum.map(&get_pair_assignents(&1))
+    |> Enum.map(&assignment_overlaps_at_all?(&1))
+    |> Enum.count(&(&1 != nil))
+  end
+
   def get_pair_assignents(input_line) do
     String.split(input_line, ",")
     |> Enum.map(&(create_range(&1)))
@@ -14,6 +21,11 @@ defmodule Day04 do
   def create_range(pair_str) do
     [from, to] = String.split(pair_str, "-")
     Range.new(from |> String.to_integer(), to |> String.to_integer())
+  end
+
+  def assignment_overlaps_at_all? ([first_assignments, second_assignments] = _pair_assignments) do
+    List.myers_difference(first_assignments |> Enum.to_list, second_assignments |> Enum.to_list)
+    |> Keyword.get(:eq)
   end
 
   def assignment_fully_overlaps?([first_assignments, second_assignments] = _pair_assignments) do
--- day04/test/day04_test.exs
+++ day04/test/day04_test.exs
@@ -8,10 +8,10 @@ defmodule Day04Test do
     assert Day04.run_part_one(input) == expected_result
   end
 
-  # test "part two" do
-  #   input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
-  #   expected_result = 2
+  test "part two" do
+    input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+    expected_result = 4
 
-  #   assert Day04.run_part_one(input) == expected_result
-  # end
+    assert Day04.run_part_two(input) == expected_result
+  end
 end
